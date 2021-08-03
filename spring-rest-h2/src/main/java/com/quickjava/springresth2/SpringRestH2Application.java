1df906af57e5a7116a7baaa4e0aca75681ff74d2
ipetroczki <391789la>
2021-08-03 21:45:10 +0200

    First version

---
+++ spring-rest-h2/src/main/java/com/quickjava/springresth2/SpringRestH2Application.java
@@ -0,0 +1,13 @@
+package com.quickjava.springresth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestH2Application.class, args);
+	}
+
+}
